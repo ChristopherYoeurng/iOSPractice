0dc2e457a8be2af4472f2857a565ad336ae128cb
ChristopherYoeurng <62725833+ChristopherYoeurng@users.noreply.github.com>
2025-02-25 16:46:16 -0800

    Lecture 1 and 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["👻", "🎃", "🦇","🧛","⚰️","🪄","🔮","🧿","🦄","🍭","🧙","🧌"]
+    @State var cardCount = 4
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            ScrollView{ cards }
+            Spacer()
+            cardCountAdjusters
         }
         .padding()
     }
+    
+    var cards: some View {
+        
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self){index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardCountAdjuster(by: -1, symbol: "minus.square.fill")
+            Spacer()
+            cardCountAdjuster(by: 1, symbol: "plus.square.fill.on.square.fill")
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster (by offset: Int, symbol: String) -> some View{
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 0 || cardCount + offset > emojis.count)
+    }
 }
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    let base = RoundedRectangle(cornerRadius: 12)
+    
+    var body: some View {
+        ZStack {
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1:0)
+            base.fill().opacity(isFaceUp ? 0:1)
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
